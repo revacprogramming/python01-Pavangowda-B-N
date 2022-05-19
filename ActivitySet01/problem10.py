f4aac4b0099149fdfbbfcdfd0c4c9a26636afede
Pavangowda-B-N <96160480+Pavangowda-B-N@users.noreply.github.com>
2022-05-19 16:42:22 +0000

    p10 is done

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,22 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+f=open("dataset/mbox-short.txt","rt")
+
+di=dict()
+largest=0
+lookvalue=None
+for i in f:
+    if i.startswith("From") and not i.startswith("From:"):
+        x=i.split()
+        fin=x[1]#find the email value
+        
+        if fin in di:
+            di[fin]=di[fin]+1
+        else:
+            di[fin]=1
+        #print(x[1])
+for k in di:
+    if largest<di[k]:
+        largest=di[k]
+        lookvalue=k;
+print(lookvalue,largest)

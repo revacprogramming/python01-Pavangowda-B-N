f4c8ffc3309ee988fc97673453078a8736b22753
Pavangowda-B-N <96160480+Pavangowda-B-N@users.noreply.github.com>
2022-05-23 03:57:37 +0000

    11 is done

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,15 @@
 # Tuples
 
-filename = "dataset/mbox-short.txt"
+f=open("dataset/mbox-short.txt","rt")
+di={}
+for i in f:
+    if i.startswith("From") and not i.startswith("From:"):
+        x=i.split()
+        y=str(x[5])
+        h=y[0:2]
+        if h in di:
+            di[h]=di[h]+1;
+        else:
+            di[h]=1
+for i in sorted (di.keys()):
+     print(i, di[i])

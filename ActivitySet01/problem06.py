6d338923b3800d5f0a617985a7200e6c5558e23e
Pavangowda-B-N <96160480+Pavangowda-B-N@users.noreply.github.com>
2022-06-13 04:02:56 +0000

    p6 is done

--- ActivitySet01/problem06.py
+++ ActivitySet01/problem06.py
@@ -1,23 +1,27 @@
-i=1;
+#Loops and Iterations
+"""
+5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+Once 'done' is entered, print out the largest and smallest of the numbers. 
+If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
+Enter 7, 2, bob, 10, and 4 and match the output below.
+"""
+i=1
 while True:
-    num=input("Enter number:");
-    if num=="done":
-        break;
-    else:
-
-            try:
-                num=int(num)
-                if i==1:
-                    largest=num;
-                    smallest=num;
-                    i=0;
-                if num>largest:
-                    largest=num;
-                elif num<smallest:
-                    smallest=num;
-            except:
-                print("Invalid input")
-                break
-                
-print("Maximum is ", largest)
-print("Minimum is ", smallest)              
+    num1=input("Enter number : ");
+    if num1=="done":
+        break
+    try:
+        num1=int(num1)
+        if i==1:
+            largest=num1
+            smallest=num1
+            i=0
+        if largest<num1:
+            largest=num1
+        elif smallest>num1:
+            smallest=num1
+    except:
+        print("Invalid input")
+        break
+print(f"Maximum is {largest}");
+print(f"Minimum is {smallest}");

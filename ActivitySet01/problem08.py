0925ab44f2d19b43841d9d3f0ca390d5f66ac5de
Pavangowda-B-N <96160480+Pavangowda-B-N@users.noreply.github.com>
2022-06-13 04:34:22 +0000

    p8 is done

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,16 +1,23 @@
 # Use the file name mbox-short.txt as the file name
-add=0;
-counter=0;
-fname = input("Enter file name: ")
-fh = open(fname,"rt")
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:"):
-        continue;
-    elif  line.startswith("X-DSPAM-Confidence:"):
-        ind=line.find("0.");
-        num=(line[ind:]);
-        float_num=float(num)
-        add+=float_num;
-        counter+=1;
-avg=add/counter;
-print(f"Average spam confidence: {avg}");
+# Use the file name mbox-short.txt as the file name
+"""
+7.2 Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+X-DSPAM-Confidence:    0.8475
+Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+You can download the sample data at http://www.py4e.com/code3/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
+"""
+fname=input("Enter file name : ")
+file=open(fname,"r")
+count=0
+addition=0
+for line in file:
+    if line.startswith("X-DSPAM-Confidence"):
+        count+=1
+        indexNo=line.find(" ")
+        y=line[indexNo:]
+        y=float(y)
+        addition+=y
+    else:
+        continue
+avg=addition/count
+print(f"Average spam confidence: {avg}")
